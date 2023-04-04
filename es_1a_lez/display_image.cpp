8b959e7f5b909464ce2ce15dddd7e12f44620e2e
Fulvioserao99 <120859540+Fulvioserao99@users.noreply.github.com>
2023-04-04 18:11:43 +0200

    Add files via upload

--- es_1a_lez/display_image.cpp
+++ es_1a_lez/display_image.cpp
@@ -4,7 +4,7 @@
 using namespace cv;
 using namespace std;
 
-Mat padding(Mat image);
+Mat padding(Mat image, int n);
 Mat blur(Mat image);
 
 
@@ -17,23 +17,18 @@ int main( int argc, char** argv )
 
     else{
 
-        puts("Usage -- ./p_name img_name\n");
+        puts("Usage -- ./p_name img_name \n");
         exit(-1);
     }
 
 
-
     Mat image;
 
+    image = imread( samples::findFile( imageName ), IMREAD_GRAYSCALE);
 
+    Mat padded = padding(image,1);
 
-    image = imread( samples::findFile( imageName ), IMREAD_GRAYSCALE );
-
-    cout << image.type() << endl;
-
-    Mat padded = padding(image);
-
-    Mat blurred = blur(padded);
+    Mat blurred = blur(image);
 
 
     if( image.empty() ){
@@ -55,61 +50,27 @@ int main( int argc, char** argv )
     return 0;
 }
 
-Mat padding(Mat image){ //padding di una immagine aggiungendo 2 righe e 2 colonne // padding per maschere standard 3x3
+Mat padding(Mat image, int n){ //padding di una immagine aggiungendo 2*n righe e 2*n colonne
 
     Mat padded;
     Scalar def = 0;
-    padded.create(image.rows+2,image.cols+2,image.type());
+    padded.create(image.rows+2*n,image.cols+2*n,image.type());
     padded.setTo(def); //settami i pixel della Mat a 0
-    image.copyTo(padded(Rect(1,1,image.cols,image.rows))); //copiami la Mat padded nel rettangolo che parte dall'elemento (1;1) e arriva all'elemento (cols-1;rows-1)
+    image.copyTo(padded(Rect(n,n,image.cols,image.rows))); //copiami la Mat padded nel rettangolo che parte dall'elemento (n;n) e arriva all'elemento (cols-1;rows-1)
 
     return padded;
 
 }
 
-Mat blur(Mat image){ //sfoca un'immagine che ha già subito un processo di padding per maschere standard 3x3
+Mat blur(Mat image){ //effettua un padding standard e sfoca l'immagine
 
-    Mat blurred;
-    Scalar def = 0;
-    blurred.create(image.rows,image.cols,image.type());
-    blurred.setTo(def);
+    Mat blurred = padding(image,1);
 
-    //poichè il metodo at è un template, dobbiamo distinguere i vari casi a seconda dell'immagine che gli passiamo
-
-    //se l'immagine è a 8bit monocanale
-    if (image.type() == CV_8UC1) {
+    if (image.type() == CV_8UC1)
     for (int i=1; i<image.rows; i++)
         for(int j=1; j<image.cols; j++){//somma i 9 pixel di una zona e metti il valore medio all'interno della nuova Mat
             blurred.at<uchar>(i, j) = (image.at<uchar>(i, j) + image.at<uchar>(i-1, j-1) + image.at<uchar>(i-1, j) + image.at<uchar>(i-1, j+1) + image.at<uchar>(i, j-1) + image.at<uchar>(i, j+1) + image.at<uchar>(i+1, j-1) + image.at<uchar>(i+1, j) + image.at<uchar>(i+1, j+1))/9;
 
-    }
-
-    //se l'immagine è a 8bit rgb
-    } else if (image.type() == CV_8UC3) {
-    for (int i=1; i<image.rows; i++)
-        for(int j=1; j<image.cols; j++){//somma i 9 pixel di una zona e metti il valore medio all'interno della nuova Mat
-            blurred.at<Vec3b>(i, j) = (image.at<Vec3b>(i, j) + image.at<Vec3b>(i-1, j-1) + image.at<Vec3b>(i-1, j) + image.at<Vec3b>(i-1, j+1) + image.at<Vec3b>(i, j-1) + image.at<Vec3b>(i, j+1) + image.at<Vec3b>(i+1, j-1) + image.at<Vec3b>(i+1, j) + image.at<Vec3b>(i+1, j+1))/9;
-
-    }
-
-    //se l'immagine è a 16bit monocanale
-    } else if (image.type() == CV_16UC1) {
-    for (int i=1; i<image.rows; i++)
-        for(int j=1; j<image.cols; j++){//somma i 9 pixel di una zona e metti il valore medio all'interno della nuova Mat
-            blurred.at<ushort>(i, j) = (image.at<ushort>(i, j) + image.at<ushort>(i-1, j-1) + image.at<ushort>(i-1, j) + image.at<ushort>(i-1, j+1) + image.at<ushort>(i, j-1) + image.at<ushort>(i, j+1) + image.at<ushort>(i+1, j-1) + image.at<ushort>(i+1, j) + image.at<ushort>(i+1, j+1))/9;
-
-    }
-
-    //se l'immagine è a 16bit rgb
-    } else if (image.type() == CV_16UC3) {
-    for (int i=1; i<image.rows; i++)
-        for(int j=1; j<image.cols; j++){//somma i 9 pixel di una zona e metti il valore medio all'interno della nuova Mat
-            blurred.at<Vec3w>(i, j) = (image.at<Vec3w>(i, j) + image.at<Vec3w>(i-1, j-1) + image.at<Vec3w>(i-1, j) + image.at<Vec3w>(i-1, j+1) + image.at<Vec3w>(i, j-1) + image.at<Vec3w>(i, j+1) + image.at<Vec3w>(i+1, j-1) + image.at<Vec3w>(i+1, j) + image.at<Vec3w>(i+1, j+1))/9;
-
-    }
-    } else {
-    cout << "Unknown img type" << endl;
-    exit(-1);
     }
 
     return blurred;
